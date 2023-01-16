d1f1ac8e5b07d3ee51eb5499d9a14eaca2e0ca7b
Anton Mikaskin <garrakombo@gmail.com>
2023-01-16 15:25:24 +0300

    refactor bx helper class

--- BX/BXHelper.php
+++ BX/BXHelper.php
@@ -1,13 +1,28 @@
 <?php
 
-class JBitrix {
+class BXHelper {
   static function includeBitrix() {
     require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
     CModule::IncludeModule("iblock");
   }
 
-  function includeFile($path, $options) {
-    $APPLICATION->IncludeFile($path, $options, ['MODE' => 'php']);
+  static function getListByParams($iblockId, $filter = [], $select = [], $order = []) {
+    CModule::IncludeModule("iblock");
+
+    $arSelect = array_merge(["ID", "IBLOCK_ID", "*", "PROPERTY_*"], $select);
+    $arFilter = array_merge(["IBLOCK_ID" => $iblockId, "ACTIVE" => "Y"], $filter);
+ 
+    $res = CIBlockElement::GetList($order, $arFilter, false, ["nPageSize"=>500], $arSelect);
+    $result = [];
+
+    while($ob = $res->GetNextElement()) {
+      array_push($result, [
+        'fields' => $ob->GetFields(),
+        'props' => $ob->GetProperties(),
+      ]);
+    }
+
+    return $result;
   }
 
   static function getSectionInfoById($sectionId, $iblockId) {
@@ -24,30 +39,6 @@ class JBitrix {
     if($uf_value = $uf_arresult->GetNext()) {
       return $uf_value;
     }
-  }
-
-  static function getInfoByIblockIdandSectionID($iblockId, $sectionId = '') {
-    CModule::IncludeModule("iblock");
-  
-    $info = [];
-  
-    $arSelect = Array('ID', 'IBLOCK_ID', '*', 'PROPERTY_*');
-    $arFilter = Array('IBLOCK_ID' => IntVal($iblockId));
-
-    if(!empty($sectionId)) {
-      array_push($arFilter, Array('IBLOCK_SECTION_ID' => intval($sectionId)));
-    }
-
-    $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
-  
-    while($ob = $res->GetNextElement()){ 
-      $info_item['props'] = $ob->GetProperties();
-      $info_item['fields'] = $ob->GetFields();
-
-      array_push($info, $info_item);
-    }
-  
-    return $info;
   }
 
   static function getResizedImagePath($id, $width = 1000, $height = 1000) {
@@ -68,5 +59,4 @@ class JBitrix {
 
     return $res;
   }
-
 }
